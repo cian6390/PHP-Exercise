247d3b71833f279b47a4d85ded0841a4d0ebd1be
Cian <cian6390@gmail.com>
2016-12-09 18:43:58 +0800

    include compoer

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,10 +4,6 @@ $app = [];
 $app['db_config'] = require 'config/database.php';
 
 require 'functions.php';
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
 
 $app['db'] = new QueryBuilder(
     Connection::make($app['db_config'])
